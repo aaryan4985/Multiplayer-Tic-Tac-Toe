fe7969c810f38d430644511b94131a5113559e2e
aaryan4985 <pradhanaaryan@gmail.com>
2024-08-24 18:23:09 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SignUp from "./components/signUp";
+import login from "./components/login";
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+      <login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/login.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    // Implement your login logic here
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login">
+      <label>Login</label>
+      <input
+        type="text"
+        placeholder="User Name"
+        value={username} // Make inputs controlled by React state
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password} // Make inputs controlled by React state
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
